ad3addcaf0e1e13fe18fbd5a78ccf082e6c56563
Agustin <rojasagustin90@gmail.com>
2021-03-23 17:42:24 -0300

    add tests and ga workflow (#1)
    * add tests and ga workflow
    
    * reduce options for os and python version
    
    * test updating poetry field
    
    * update poetry config
    
    * rm package option
    
    * refactor directory
    
    * test
    
    * test
    
    * test file

--- app/api/users_api.py
+++ app/api/users_api.py
@@ -1,12 +1,16 @@
+import logging
+
 from fastapi import APIRouter, Depends
 from typing import List
 
-from api.models.user import UserResponse
-from services.users_service import UsersService
+from app.api.models.user import UserResponse
+from app.services.users_service import UsersService
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.get('/api/users', response_model=List[UserResponse])
 async def get_users(service: UsersService = Depends(UsersService)):
+    logger.info("Get all users called")
     return service.get_users()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,16 @@
-from api import __version__
+from app.api import __version__
+from fastapi.testclient import TestClient
+from main import api
+
+client = TestClient(api)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_get_users():
+    response = client.get("/api/users")
+    assert response.status_code == 200
+    assert response.json()
+
