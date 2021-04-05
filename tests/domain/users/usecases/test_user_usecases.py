82480433204a2176888d8f7e42fca5c7d251e6d6
Agustin Rojas <rojasagustin90@gmail.com>
2021-04-04 23:52:01 -0300

    fix test

--- tests/domain/users/usecases/test_user_usecases.py
+++ tests/domain/users/usecases/test_user_usecases.py
@@ -7,25 +7,30 @@ from app.domain.users.model.user import User
 from app.domain.users.usecases.user_usecases import UserUseCases
 
 
-class MyTestCase(unittest.TestCase):
-    repository = InMemoryUserRepository()
+class TestUserUseCases(unittest.TestCase):
+    db = {}
+    repository = InMemoryUserRepository(db)
+    pwd_context = MagicMock()
 
     def test_list_empty(self):
-        user_usecases = UserUseCases(self.repository)
+        user_usecases = UserUseCases(self.repository, self.pwd_context)
         self.repository.all = MagicMock(return_value=[])
         self.assertEqual(user_usecases.list(), [])
 
     def test_list_with_results(self):
-        user_mock = User(username='mock', email='email@mail.com')
+        user_mock = User(username='mock', email='email@mail.com', password='aaaa')
         self.repository.all = MagicMock(return_value=[user_mock])
-        user_usecases = UserUseCases(self.repository)
+        user_usecases = UserUseCases(self.repository, self.pwd_context)
         self.assertEqual(user_usecases.list(), [user_mock])
 
     def test_register(self):
-        user_mock = User(username='mock', email='email@mail.com')
+        user_mock = User(username='mock', email='email@mail.com', password='aaaa')
         self.repository.save = MagicMock(return_value=user_mock)
-        user_usecases = UserUseCases(self.repository)
-        user_register = UserCreateCommand(username='mock', email='email@mail.com')
+        self.pwd_context.hash = MagicMock(return_value='aaaa')
+        user_usecases = UserUseCases(self.repository, self.pwd_context)
+        user_register = UserCreateCommand(
+            username='mock', email='email@mail.com', password='1234'
+        )
         self.assertIsNotNone(user_usecases.register(user_register))
 
 
