b71ca17ffe06d51db3d89b873e62acda2d150ff1
Agustin Rojas <rojasagustin90@gmail.com>
2021-04-05 00:09:32 -0300

    rm unused code

--- app/domain/users/usecases/user_authentication_usecases.py
+++ app/domain/users/usecases/user_authentication_usecases.py
@@ -1,4 +1,4 @@
-from passlib.context import CryptContext
+from typing import Optional
 
 from app.domain.users.command.user_credentials_command import UserCredentialsCommand
 from app.domain.users.model.user import User
@@ -10,12 +10,14 @@ class UserAuthenticationUseCases:
         self.user_repository = user_repository
         self.pwd_context = pwd_context
 
-    def find_by_credentials(self, user_command: UserCredentialsCommand) -> User:
+    def find_by_credentials(
+        self, user_command: UserCredentialsCommand
+    ) -> Optional[User]:
         user = self.user_repository.find_by_username(user_command.username)
         if not user:
-            return False
+            return None
         if not self.verify_password(user_command.password, user.password):
-            return False
+            return None
         return user
 
     def verify_password(self, plain_password, hashed_password):
@@ -23,15 +25,3 @@ class UserAuthenticationUseCases:
 
     def get_password_hash(self, password):
         return self.pwd_context.hash(password)
-
-    # def get_user(db, username: str):
-    #     if username in db:
-    #         user_dict = db[username]
-    #         return UserInDB(**user_dict)
-    #
-    #
-    # def fake_decode_token(token):
-    #     # This doesn't provide any security at all
-    #     # Check the next version
-    #     user = get_user(fake_users_db, token)
-    #     return user
