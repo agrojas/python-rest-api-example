a0e0b7d1e031ef58d458390a5f1cc519504f9b13
Agustin Rojas <rojasagustin90@gmail.com>
2021-07-18 19:27:18 -0300

    Increase coverage

--- tests/adapters/database/users/test_sql_user_repository.py
+++ tests/adapters/database/users/test_sql_user_repository.py
@@ -78,6 +78,24 @@ class TestSQLUserRepository(unittest.TestCase):
 
         assert user_repository.find_by_username("tesst5") == user2
 
+    def test_user_repository_find_by_username_not_found(self):
+        user_repository = SQLUserRepository(self.session)
+        User(
+            id=UserId(id=str(uuid.uuid4())),
+            username="tesst4",
+            email="test4@mail.com",
+            password='aaaa',
+        ).save(user_repository)
+        user2 = User(
+            id=UserId(id=str(uuid.uuid4())),
+            username="tesst5",
+            email="test5@mail.com",
+            password='aaaa',
+        )
+        user2.save(user_repository)
+
+        assert user_repository.find_by_username("tesstWrong") is None
+
     def test_user_repository_find_by_id(self):
         user_repository = SQLUserRepository(self.session)
         id_user_1 = UserId(id=str(uuid.uuid4()))
@@ -94,3 +112,20 @@ class TestSQLUserRepository(unittest.TestCase):
         user2.save(user_repository)
 
         assert user_repository.find_by_id(id_user_1) == user1
+
+    def test_user_repository_find_by_id_not_found(self):
+        user_repository = SQLUserRepository(self.session)
+        id_user_1 = UserId(id=str(uuid.uuid4()))
+        wrong_id_user = UserId(id=str(uuid.uuid4()))
+        user1 = User(
+            id=id_user_1, username="tesst4", email="test4@mail.com", password='aaaa',
+        )
+        user1.save(user_repository)
+        user2 = User(
+            id=UserId(id=str(uuid.uuid4())),
+            username="tesst5",
+            email="test5@mail.com",
+            password='aaaa',
+        )
+        user2.save(user_repository)
+        assert user_repository.find_by_id(wrong_id_user) is None
