aba4a48c9a4db28f47aa607078aaa45d7a589d94
Agustin Rojas <rojasagustin90@gmail.com>
2021-07-18 19:20:23 -0300

    Increase coverage

--- tests/adapters/database/users/test_sql_user_repository.py
+++ tests/adapters/database/users/test_sql_user_repository.py
@@ -59,3 +59,38 @@ class TestSQLUserRepository(unittest.TestCase):
         ).save(user_repository)
 
         assert user_repository.total() == 2
+
+    def test_user_repository_find_by_username(self):
+        user_repository = SQLUserRepository(self.session)
+        User(
+            id=UserId(id=str(uuid.uuid4())),
+            username="tesst4",
+            email="test4@mail.com",
+            password='aaaa',
+        ).save(user_repository)
+        user2 = User(
+            id=UserId(id=str(uuid.uuid4())),
+            username="tesst5",
+            email="test5@mail.com",
+            password='aaaa',
+        )
+        user2.save(user_repository)
+
+        assert user_repository.find_by_username("tesst5") == user2
+
+    def test_user_repository_find_by_id(self):
+        user_repository = SQLUserRepository(self.session)
+        id_user_1 = UserId(id=str(uuid.uuid4()))
+        user1 = User(
+            id=id_user_1, username="tesst4", email="test4@mail.com", password='aaaa',
+        )
+        user1.save(user_repository)
+        user2 = User(
+            id=UserId(id=str(uuid.uuid4())),
+            username="tesst5",
+            email="test5@mail.com",
+            password='aaaa',
+        )
+        user2.save(user_repository)
+
+        assert user_repository.find_by_id(id_user_1) == user1
